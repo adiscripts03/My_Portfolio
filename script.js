33b4c2b210943b44245c5fad382b2a7a1e029a65
adiscripts03 <mejustaditya03.in>
2026-03-24 20:46:53 +0530

    Initial portfolio commit

---
+++ script.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+    VANTA.NET({
+        el: "#vanta-bg",
+        mouseControls: true,
+        touchControls: true,
+        color: 0x00f5a0,
+        backgroundColor: 0x000000,
+        points: 12,
+        maxDistance: 20,
+        spacing: 15
+    });
+};
